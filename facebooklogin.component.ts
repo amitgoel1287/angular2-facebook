a4d63ba6bf974f1b272ac4d4a3ba328f1031ea81
Amit Goel <amitgoel1287@gmail.com>
2016-04-15 02:49:04 +0530

    initial check-in

---
+++ facebooklogin.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from 'angular2/core';
+import {ROUTER_DIRECTIVES, Router} from "angular2/router";
+
+
+declare const FB:any;
+
+@Component({
+    selector: 'cp-facebook-login',
+    templateUrl: 'app/careerplot/app/auth/views/facebooklogin.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class FacebookLoginComponent implements OnInit {
+
+    constructor() {
+        FB.init({
+            appId      : 'your-app-id',
+            cookie     : false,  // enable cookies to allow the server to access
+                                // the session
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.5' // use graph api version 2.5
+        });
+    }
+
+    onFacebookLoginClick() {
+        FB.login();
+    }
+
+    statusChangeCallback(resp) {
+        if (resp.status === 'connected') {
+            // connect here with your server for facebook login by passing access token given by facebook
+        }else if (resp.status === 'not_authorized') {
+            
+        }else {
+            
+        }
+    };
+    ngOnInit() {
+        FB.getLoginStatus(response => {
+            this.statusChangeCallback(response);
+        });
+    }
+}
